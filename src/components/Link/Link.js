6d309f795d156de95be0e09675293e2a7f501d93
Manuel Pascual <manupascual90@gmail.com>
2020-06-22 15:10:29 -0300

    Add icon prop

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./link.scss";
 
-const Link = ({ text, url, colorVariant = "muted" }) => {
+const Link = ({ text, url, colorVariant = "muted", icon = null }) => {
   return (
     <a
       className={`link ${colorVariant}`}
@@ -11,6 +11,7 @@ const Link = ({ text, url, colorVariant = "muted" }) => {
       target="_blank"
     >
       {text}
+      {icon && icon()}
     </a>
   );
 };
@@ -19,6 +20,7 @@ Link.propTypes = {
   text: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
   colorVariant: PropTypes.oneOf(["absence", "muted"]),
+  icon: PropTypes.func,
 };
 
 export default Link;
