8298e7b0b8f2f42069fb5d48293804f25f7d63d6
Manuel Pascual <manupascual90@gmail.com>
2020-06-23 13:12:40 -0300

    Create SectionPackages component

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -3,6 +3,7 @@ import React from "react";
 import SectionAbout from "./SectionAbout";
 import SectionWork from "./SectionWork";
 import SectionArticles from "./SectionArticles";
+import SectionPackages from "./SectionPackages";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <SectionAbout />
       <SectionWork />
       <SectionArticles />
+      <SectionPackages />
     </>
   );
 };
---
+++ src/views/Home/SectionPackages/SectionPackages.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+import ViewSection from "../../../components/ViewSection";
+import SectionTitle from "../../../components/SectionTitle";
+import Text from "../../../components/Text";
+import Grid from "../../../components/Grid";
+import Package from "../../../components/Package";
+
+const packages = [
+  {
+    title: "alfil",
+    description: "Data validation JS library.",
+    url: "https://www.npmjs.com/package/alfil",
+    color: "#00cc99",
+  },
+  {
+    title: "pswmeter",
+    description: "Password strength meter.",
+    url: "https://github.com/pascualmj/pswmeter",
+    color: "#3366ff",
+  },
+  {
+    title: "mpselect",
+    description: "Custom dropdown list jQuery plugin.",
+    url: "https://github.com/pascualmj/mpselect",
+    color: "yellow",
+  },
+];
+
+const SectionPackages = () => {
+  return (
+    <ViewSection>
+      <SectionTitle title="Open source projects." size="regular" />
+      <Text className="mt-2 mb-7">
+        I’m Manuel Pascual, a Full Stack Web Developer based in Argentina. With
+        every line of code I write, I strive to make the web a beatiful place
+        lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </Text>
+      <Grid
+        items={packages}
+        keyExtractor={(item) => item.title}
+        renderItems={(item) => (
+          <Package
+            title={item.title}
+            description={item.description}
+            url={item.url}
+            color={item.color}
+            className="mb-3"
+          />
+        )}
+        columns={3}
+      />
+    </ViewSection>
+  );
+};
+
+export default SectionPackages;
