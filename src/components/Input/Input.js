8e193b86b7a8a3385d109f90396a2d26248cdea9
Manuel Pascual <manupascual90@gmail.com>
2020-06-23 15:23:08 -0300

    Create Input component

---
+++ src/components/Input/Input.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./input.scss";
+
+const Input = ({
+  type,
+  value = "",
+  handleChange,
+  placeholder = "",
+  icon = "",
+  className = "",
+}) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
+  return (
+    <div
+      className={`field-input ${isFocused ? "field-focused" : ""} ${className}`}
+    >
+      {icon && <i class={`${icon} fa-fw`}></i>}
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      />
+    </div>
+  );
+};
+
+Input.propTypes = {
+  type: PropTypes.oneOf(["text", "email", "number", "password"]).isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  handleChange: PropTypes.func.isRequired,
+  icon: PropTypes.string,
+  className: PropTypes.string,
+};
+
+export default Input;
