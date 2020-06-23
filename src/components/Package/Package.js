45440abae3f69f4bee4cb3d4106213ea720ff2a3
Manuel Pascual <manupascual90@gmail.com>
2020-06-23 13:12:21 -0300

    Create Package component

---
+++ src/components/Package/Package.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./package.scss";
+
+import Text from "../Text";
+
+const Package = ({
+  title,
+  description,
+  url,
+  color = "#999",
+  className = "",
+}) => {
+  return (
+    <a href={url} rel="noopener noreferrer" className={`package ${className}`}>
+      <article>
+        <span className="package-circle">
+          <span style={{ backgroundColor: color }}></span>
+        </span>
+        <div className="package-body">
+          <Text color="absence" font="secondary" size="small">
+            {title}
+          </Text>
+          <Text style={{ color }} size="smaller">
+            {description}
+          </Text>
+        </div>
+      </article>
+    </a>
+  );
+};
+
+Package.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  className: PropTypes.string,
+};
+
+export default Package;
