88d34320df988804247e97dc652b783e51b89fe4
Manuel Pascual <manupascual90@gmail.com>
2020-06-25 21:20:36 -0300

    Add FormContact component

--- src/views/Home/SectionContact/SectionContact.js
+++ src/views/Home/SectionContact/SectionContact.js
@@ -3,16 +3,9 @@ import React from "react";
 import ViewSection from "../../../components/ViewSection";
 import SectionTitle from "../../../components/SectionTitle";
 import Text from "../../../components/Text";
-import Input from "../../../components/Input";
-import Textarea from "../../../components/Textarea";
-import Form from "../../../components/Form";
-import Button from "../../../components/Button";
+import FormContact from "../../../components/FormContact";
 
 const SectionContact = () => {
-  const handleSubmit = (values) => {
-    console.log(values);
-  };
-
   return (
     <ViewSection>
       <SectionTitle title="Contact Me." size="regular" />
@@ -21,43 +14,7 @@ const SectionContact = () => {
         every line of code I write, I strive to make the web a beatiful place
         lorem ipsum dolor sit amet, consectetur adipiscing elit.
       </Text>
-      <Form
-        initialValues={{
-          fieldName: "",
-          fieldEmail: "",
-          fieldMessage: "",
-        }}
-        handleSubmit={handleSubmit}
-      >
-        {({ values, handleChange }) => (
-          <>
-            <Input
-              type="text"
-              placeholder="Your name is..."
-              value={values.fieldName}
-              handleChange={handleChange("fieldName")}
-              icon="fas fa-user"
-              className="mb-2"
-            />
-            <Input
-              type="email"
-              placeholder="Your email is..."
-              value={values.fieldEmail}
-              handleChange={handleChange("fieldEmail")}
-              icon="fas fa-envelope"
-              className="mb-2"
-            />
-            <Textarea
-              placeholder="Leave your message..."
-              value={values.fieldMessage}
-              handleChange={handleChange("fieldMessage")}
-              icon="fas fa-comment"
-              className="mb-3"
-            />
-            <Button type="submit" text="Send message" size="big" />
-          </>
-        )}
-      </Form>
+      <FormContact />
     </ViewSection>
   );
 };
