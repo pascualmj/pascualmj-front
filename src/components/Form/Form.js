a5e21622d72ad5f0dc5e8409e45ae3879f3d1189
Manuel Pascual <manupascual90@gmail.com>
2020-06-25 21:19:31 -0300

    Add more validations and handlers

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const Form = ({ initialValues, handleSubmit, children }) => {
+const Form = ({
+  initialValues,
+  handleSubmit,
+  dataValidator = null,
+  children,
+}) => {
   const [values, setValues] = useState(initialValues);
+  const [errors, setErrors] = useState(initializeErrors(initialValues));
+
+  function initializeErrors(fields) {
+    const result = {};
+    for (let key in fields) {
+      result[key] = fields[key];
+    }
+    return result;
+  }
 
   const handleChange = (name) => {
     return (e) => {
@@ -16,14 +30,31 @@ const Form = ({ initialValues, handleSubmit, children }) => {
     };
   };
 
+  const runValidations = () => {
+    if (!dataValidator) return;
+    const { isValid, data } = dataValidator.validate(values);
+    for (let key in data) {
+      setErrors((currentErrors) => {
+        return {
+          ...currentErrors,
+          [key]: data[key].errors ? data[key].errors[0] : "",
+        };
+      });
+    }
+    return isValid;
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!runValidations()) return;
+    handleSubmit(values, (newValues) => {
+      setValues(newValues);
+    });
+  };
+
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleSubmit(values);
-      }}
-    >
-      {children({ values, handleChange })}
+    <form onSubmit={submitHandler} noValidate>
+      {children({ values, handleChange, errors })}
     </form>
   );
 };
@@ -31,6 +62,8 @@ const Form = ({ initialValues, handleSubmit, children }) => {
 Form.propTypes = {
   initialValues: PropTypes.object.isRequired,
   handleSubmit: PropTypes.func.isRequired,
+  dataValidator: PropTypes.object,
+  children: PropTypes.any,
 };
 
 export default Form;
