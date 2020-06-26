58114b793ffced7b97d94d3edf2c4553016a60c9
Manuel Pascual <manupascual90@gmail.com>
2020-06-25 21:14:53 -0300

    Create constants file

---
+++ src/config/constants.js
@@ -0,0 +1,7 @@
+export const successMessage = "Message sent successfully!";
+export const errorMessage = "Ups! Something went wrong, try again later.";
+export const fieldErrorText = {
+  fieldName: "This field is required.",
+  fieldEmail: "You must enter a valid email address.",
+  fieldMessage: "Don't forget to leave your message :)",
+};
