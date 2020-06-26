014b5bfcbc644d97231d91772c1ee3397f18b9bd
Manuel Pascual <manupascual90@gmail.com>
2020-06-26 19:48:56 -0300

    Export isInViewport function

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,5 +1,6 @@
 import formatItemSpacing from "./formatItemSpacing";
 import renderListDivider from "./renderListDivider";
 import scrollToSection from "./scrollToSection";
+import isInViewport from "./isInViewport";
 
-export { formatItemSpacing, renderListDivider, scrollToSection };
+export { formatItemSpacing, renderListDivider, scrollToSection, isInViewport };
