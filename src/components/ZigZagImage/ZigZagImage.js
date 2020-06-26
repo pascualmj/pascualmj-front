9e89ef4665b5eb7428ed13f9fca8719d382dcc46
Manuel Pascual <manupascual90@gmail.com>
2020-06-26 15:20:32 -0300

    Add variant prop

--- src/components/ZigZagImage/ZigZagImage.js
+++ src/components/ZigZagImage/ZigZagImage.js
@@ -1,13 +1,25 @@
 import React from "react";
-import zigZagImage from "../../assets/img/img_02.png";
+import PropTypes from "prop-types";
+import zigZagImagePrimary from "../../assets/img/img_02.png";
+import zigZagImageDarkLighter from "../../assets/img/img_03.png";
 import "./zigZagImage.scss";
 
-const ZigZagImage = () => {
+const ZigZagImage = ({ variant = "primary" }) => {
   return (
     <>
-      <img className="zig-zag-image" src={zigZagImage} alt="" />
+      <img
+        className="zig-zag-image"
+        src={
+          variant === "primary" ? zigZagImagePrimary : zigZagImageDarkLighter
+        }
+        alt=""
+      />
     </>
   );
+};
+
+ZigZagImage.propTypes = {
+  variant: PropTypes.oneOf(["primary", "dark-lighter"]),
 };
 
 export default ZigZagImage;
